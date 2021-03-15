38816f72b611421931e293105f7148701cf525d0
Salman Pigi <C010321004@Supporting.corp.bankmandiri.co.id>
2021-03-15 07:06:56 +0700

    Added gateway service

---
+++ api-gateway-zuul/src/main/java/id/co/bmri/induction/api/gateway/zuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package id.co.bmri.induction.api.gateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ api-gateway-zuul/src/test/java/id/co/bmri/induction/api/gateway/zuul/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bmri.induction.api.gateway.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
