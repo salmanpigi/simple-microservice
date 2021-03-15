f6cc86c0e83844b16224b34401e2316cb9734afd
Salman Pigi <C010321004@Supporting.corp.bankmandiri.co.id>
2021-03-15 07:15:58 +0700

    Added discovery service

---
+++ eureka-service/src/test/java/co/id/bmri/induction/service/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.bmri.induction.service.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
